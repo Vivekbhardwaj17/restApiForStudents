96972940888ed74b72b18afb5f777e48139ce2cf
Vivek Bhardwaj <48515224+Vivekbhardwaj17@users.noreply.github.com>
2021-06-10 13:04:23 +0530

    Update StudentController.java
    added some more controllers

--- src/main/java/com/springexample/SpringRestApi/controller/StudentController.java
+++ src/main/java/com/springexample/SpringRestApi/controller/StudentController.java
@@ -3,10 +3,7 @@ package com.springexample.SpringRestApi.controller;
 import com.springexample.SpringRestApi.model.Student;
 import com.springexample.SpringRestApi.service.studentoperations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,14 +17,40 @@ public class StudentController {
     public String hello(){
         return "Hello! you are on correct page";
     }
+
+    @GetMapping("/hellos")
+    public String helloRes(@RequestParam String str){
+        return str +"is a university";
+    }
+
     @GetMapping("/students")
     public List<Student> getAllStudent(){
         return stud.getAllStudents();
     }
 
-    @GetMapping("/student/{studentId}")
+    @GetMapping("/students/{studentId}")
     public Student getStudentById(@PathVariable String studentId){
         return stud.getById(Long.parseLong(studentId));
+    }
+
+    @GetMapping("/students/{departmentId}")
+    public List<Student> getStudentByDept(@PathVariable String departmentId){
+        return stud.getByDepartment(departmentId);
+    }
+
+    @PostMapping("/students")
+    public Student addStudent(@RequestBody Student student){
+        return stud.addStudent(student);
+    }
+
+    @PutMapping("/students")
+    public Student updateStudent(@RequestBody Student student){
+        return stud.updateStudent(student);
+    }
+
+    @DeleteMapping("/students/{studentId}")
+    public List<Student> deleteStudent(@PathVariable String studentId ){
+        return stud.removeStudent(Long.parseLong(studentId));
     }
 
 }
