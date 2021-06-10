03b514916925e819ef783a25695aebef1d9a4e0d
Vivek Bhardwaj <48515224+Vivekbhardwaj17@users.noreply.github.com>
2021-06-10 13:05:50 +0530

    Update studentoperations.java

--- src/main/java/com/springexample/SpringRestApi/service/studentoperations.java
+++ src/main/java/com/springexample/SpringRestApi/service/studentoperations.java
@@ -8,7 +8,9 @@ import java.util.List;
 @Service
 public interface studentoperations {
     public List<Student> getAllStudents();
-    public List<Student> getByDepartment();
+    public List<Student> getByDepartment(String departmentId);
     public Student getById(Long studentId);
-
+    public Student addStudent(Student student);
+    public Student updateStudent(Student student);
+    public List<Student> removeStudent(Long studentId);
 }
